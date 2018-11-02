a44cc91eb43abe3bbdea8bc747d799b47f8d1f9a
s645robin <s645robin@gmail.com>
2018-11-02 13:09:24 +0530

    .

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ import Heading from './components/heading';
 import Icon from './components/icon';
 import Label from './components/label';
 import Loader from './components/loader';
-import QRCodeScan from './components/qr_code_scan';
 import RadioChoices from './components/radio_choices';
 import RadioForm from './components/radio_form';
 import Separator from './components/separator';
@@ -65,7 +64,6 @@ export {
   Icon,
   Label,
   Loader,
-  QRCodeScan,
   RadioChoices,
   RadioForm,
   Separator,
