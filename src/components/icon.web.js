e8822ae8ff17dd9477293ef9422a0a16251ba1b0
s645robin <s645robin@gmail.com>
2019-01-09 16:12:40 +0530

    .

---
+++ src/components/icon.web.js
@@ -0,0 +1,93 @@
+import React from 'react'
+
+import ZocialIcon from 'react-native-vector-icons/dist/Zocial'
+import MaterialIcon from 'react-native-vector-icons/dist/MaterialIcons'
+import MaterialCommunityIcon from 'react-native-vector-icons/dist/MaterialCommunityIcons'
+import EntypoIcon from 'react-native-vector-icons/dist/Entypo'
+import FAIcon from 'react-native-vector-icons/dist/FontAwesome'
+import IonIcon from 'react-native-vector-icons/dist/Ionicons'
+import EvilIcon from 'react-native-vector-icons/dist/EvilIcons'
+
+const style = document.createElement('style');
+style.type = 'text/css';
+style.appendChild(
+	document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/Zocial.ttf`)});
+			font-family: Zocial;
+		}`
+	)
+)
+style.appendChild(
+  document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/MaterialIcons.ttf`)});
+			font-family: MaterialIcons;
+		}`
+	)
+)
+style.appendChild(
+  document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf`)});
+			font-family: MaterialCommunityIcons;
+		}`
+	)
+)
+style.appendChild(
+  document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/Entypo.ttf`)});
+			font-family: Entypo;
+		}`
+	)
+)
+style.appendChild(
+  document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/FontAwesome.ttf`)});
+			font-family: FontAwesome;
+		}`
+	)
+)
+style.appendChild(
+  document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/Ionicons.ttf`)});
+			font-family: Ionicons;
+		}`
+	)
+)
+style.appendChild(
+  document.createTextNode(
+		`@font-face {
+			src: url(${require(`react-native-vector-icons/Fonts/EvilIcons.ttf`)});
+			font-family: EvilIcons;
+		}`
+	)
+)
+
+document.head.appendChild(style)
+
+const AllIcons = {
+  'font-awesome': FAIcon,
+  'entypo': EntypoIcon,
+  'material-community': MaterialCommunityIcon,
+  'material-icon': MaterialIcon,
+  'zocial': ZocialIcon,
+  'ion': IonIcon,
+  'evil': EvilIcon
+}
+
+export default class Icon extends React.PureComponent {
+  render () {
+    const IconComponent = AllIcons[this.props.type]
+    let element
+    if (IconComponent) {
+      element = <IconComponent {...this.props} />
+    } else {
+      element = null
+    }
+    return element
+  }
+}
---
+++ src/index.web.js
@@ -0,0 +1,9 @@
+import Icon from './components/icon'
+import SubTitle from './components/sub_title'
+import Title from './components/title'
+
+export {
+  Icon,
+  SubTitle,
+  Title
+}
