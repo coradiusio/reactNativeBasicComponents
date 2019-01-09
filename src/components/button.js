34b910c6b98d9d120f8cf688ae9ece93a04fc0c9
s645robin <s645robin@gmail.com>
2019-01-09 17:27:03 +0530

    .

--- src/components/button.js
+++ src/components/button.js
@@ -2,22 +2,31 @@ import React from 'react'
 
 import {
   View,
+  Text,
   StyleSheet
 } from 'react-native'
 
 import {
-  Button
-} from 'react-native-material-ui'
+  MKButton
+} from 'react-native-material-kit'
 
 export default class ButtonComponent extends React.PureComponent {
   render () {
     return (
       <View style={[styles.buttonContainer, this.props.buttonContainerStyle]}>
-        <Button
-          raised
-          style={this.props.style}
+        <MKButton
+          backgroundColor={this.props.buttonBackgroundColor}
+          shadowRadius={2}
+          shadowOffset={styles.buttonShadowOffset}
+          shadowOpacity={.7}
+          shadowColor='black'
+          style={[styles.button, this.props.buttonStyle]}
           {...this.props}
-        />
+          >
+          <Text pointerEvents='none' style={[styles.text, this.props.textStyle]}>
+            {this.props.text.toUpperCase()}
+          </Text>
+        </MKButton>
       </View>
     )
   }
@@ -25,6 +34,20 @@ export default class ButtonComponent extends React.PureComponent {
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    padding: 16
+    padding: 16,
+  },
+  buttonShadowOffset: {
+    width: 0,
+    height: 2
+  },
+  button: {
+    height: 48,
+    elevation: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold'
   }
 })
